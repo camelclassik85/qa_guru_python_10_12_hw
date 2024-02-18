c0cd0fd9e7fc7ddcda2967ea8ab179c5147bea3c
Alexander_Dulalaev <camel1985@mail.ru>
2024-02-18 23:07:23 +0300

    upd abth path to picture

--- qa_guru_python_10_10/resource.py
+++ qa_guru_python_10_10/resource.py
@@ -3,4 +3,4 @@ import tests
 
 
 def path(file_name):
-    return str(Path(__file__).parent.parent.joinpath(f'tests/{file_name}').absolute())
+    return str(Path(__file__).parent.joinpath(f'tests/{file_name}').absolute())
